f45a3f0cd84d54c248661a6d57d39fd7aec77ff4
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-10-28 09:07:39 +0530

    deleted temprunner file

--- talkingComputer/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-let textarea = document.querySelector("textarea");
-// let speed = document.querySelector("#speed").value;
-// let playbtn = document.querySelector(".play");
-// let stopbtn = document.querySelector(".stop");
