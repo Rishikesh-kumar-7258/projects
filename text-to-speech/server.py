edc42c83335a3a008a9a4b29991a625587d93abc
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-12-08 15:58:20 +0530

    Basic layour completed

--- text-to-speech/server.py
+++ text-to-speech/server.py
@@ -3,6 +3,7 @@ from flask.templating import render_template
 import gtts
 from playsound import playsound
 from flask import Flask, request
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -11,11 +12,18 @@ def home():
 
     if request.method == 'POST':
         text = request.form['text']
-        tts = gtts.gTTS(text=text, lang='en')
+        tts = gtts.gTTS(text=text, lang='hi')
         tts.save('static/sound/result.mp3')
-    # playsound("welcome.mp3")
+
+        return redirect("/playSound")
+
+    else:
         return render_template("index.html")
-    return render_template("index.html")
+    
+@app.route('/playSound')
+def playSound():
+
+    return render_template('playSound.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
